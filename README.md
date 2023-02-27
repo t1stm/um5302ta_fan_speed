82278092afd71bbfdc90c6741166b56034ca9a49
Kristian Gergov <kristian.gergov1@gmail.com>
2023-02-27 11:06:39 +0200

    Add the script. Edit README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # um5302ta_fan_speed
-A small script to control the fan.
+A small script to control the fan of the Asus Zenbook S 13 OLED UM5302TA.
+
+---
+
+## Usage.
+
+```./fan-speed.sh -s performance```
+
+Available modes are "performance" / "high", "normal" / "mid" / "default", "whisper" / "low".
---
+++ fan-speed.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# Control the fans using EC-Probe
+# Adresses found using ec-probe.exe in Windows 11.
+# Used the MyASUS app.
+
+Mode=""
+EC_Address="0x61"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -s|--speed)
+            Mode="$2"
+            shift
+            shift
+            ;;
+        -d|--default)
+            Mode="default"
+            shift
+            shift
+            ;;
+        *)
+            echo "Parameter \'$1\' not found." >&2;
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$Mode" = "" ]; then
+	echo "No mode specified. Exiting." >&2;
+	echo "
+Available parameters are:
+-s / --speed \"(performance or high), (normal, mid or default) and (whisper or low)\"
+-d / --default" >&2;
+	exit 1;
+fi
+
+EC_Probe_Location="$(which ec-probe)"
+
+if [ $? -ne 0 ]; then 
+	echo "NBFC EC-Probe isn't installed on the system. Please make sure it's installed, and present in \$PATH" >&2;
+	exit 1;
+fi
+
+case $Mode in 
+	performance|high)
+		sudo "$EC_Probe_Location" write "$EC_Address" 0x02
+		;;
+	normal|mid|default)
+		sudo "$EC_Probe_Location" write "$EC_Address" 0x00
+		;;
+	whisper|low)
+		sudo "$EC_Probe_Location" write "$EC_Address" 0x01
+		;;
+	*)
+		echo "Invalid fan speed parameter. Available options are \"(performance or high), (normal, mid or default) and (whisper or low)\"" >&2
+		;;
+esac
+exit 0
